caffe65ae67a49b6c0fe213588563d57a1bb1098
Lucas Caribé <lucasmoc47@gmail.com>
2021-11-06 01:46:32 -0300

    Using viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,7 @@
-from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-  path('welcome', views.welcome),
-  path('getexpenses/', views.get_expenses),
-  path('addexpense/', views.add_expense),
-  path('editexpense/<int:expense_id>', views.edit_expense),
-  path('deleteexpense/<int:expense_id>', views.delete_expense),
-]
+router = DefaultRouter()
+router.register(r'expenses', views.ExpenseView)
+
+urlpatterns = router.urls
